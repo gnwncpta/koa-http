41c0e644e88de7b6203b939e21cc221aad3fc9d1
gnwncpta <gnwncpta@gmail.com>
2022-07-07 21:12:47 +0700

    update: add /randomQuotes

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 const Koa = require('koa');
 const app = new Koa();
 
-app.use(async (ctx) => {
+function index(ctx){
     const data = { 
         code: 200, 
         message: "WELCOME TO KOA HTTP SERVER",
@@ -12,6 +12,37 @@ app.use(async (ctx) => {
     const body = JSON.stringify(data);
     ctx.status = 200;
     ctx.body = body;
+}
+
+function randomQuotes(ctx){
+    const quotes = [
+        { quote: "GG GAMING GAK GAES!???", author: "Windah Basudara" },
+        { quote: "SIAPA YANG CITA CITANYA ???", author: "Windah Basudara" },
+        { quote: "WELCOME DI TAG BLASSSTTT KKRGGHHHH :V", author: "Tara Arts & Gema" },
+        { quote: "Kembali lagi yaa bersama sama host yang ganteng disini :D", author: "Tara Arts" },
+        { quote: "Yo guys! balik lagi sama gw di Detective Aldo", author: "Detective Aldo" },
+    ];
+
+    const pickedQuote = quotes[Math.floor(Math.random() * quotes.length)];
+    const data = { code: 200, result: pickedQuote };
+    const body = JSON.stringify(data);
+    ctx.status = 200;
+    ctx.body = body;
+}
+
+app.use(async (ctx) => {
+    const { request } = ctx;
+    
+    switch(request.url){
+        case "/":
+            index(ctx);
+
+        case "/randomQuotes":
+            randomQuotes(ctx);
+
+        default:
+            return new Error();
+    }
 });
 
 app.listen(process.env.PORT || 3000, () => console.log("Server running on port 3000"));
